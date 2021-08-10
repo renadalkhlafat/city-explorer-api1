1e8c253c3454548a8c6b529c0f44eb0bee3f1615
renadalkhlafat <renadsalem8888@gmail.com>
2021-08-11 01:33:37 +0300

    weather and movies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios')
 const cors = require('cors');
 const weatherjson = require('./data/weather.json');
 const server = express();
@@ -23,9 +24,9 @@ server.get('/weather', (req, res) => {
 server.get('/weather/:lon/:lat/:city_name', (req, res) => {
     const data = weatherjson.find(
         (ele) =>
-        +ele.lon === +req.params.lon &&
-        +ele.lat === +req.params.lat &&
-        ele.city_name === req.params.city_name
+            +ele.lon === +req.params.lon &&
+            +ele.lat === +req.params.lat &&
+            ele.city_name === req.params.city_name
     );
     data
         ?
@@ -48,9 +49,66 @@ server.get('/weather/:city_name', (req, res) => {
     }
 });
 
+server.get('/weather/:lon/:lat', handleWeather);
+server.get('/movies/:query', moviesHandler);
+
+function handleWeather(req, res) {
+    const weatherKey = process.env.WEATHER_API_KEY;
+    const resUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${+req
+        .params.lat}&lon=${+req.params.lon}&key=${weatherKey}`;
+    let weatherArr = [];
+    axios
+        .get(resUrl)
+        .then((weather) => {
+            weather.data.data.map((day) => {
+                weatherArr.push(new Forecast(day));
+            });
+            res.send(weatherArr);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+}
+
+function moviesHandler(req, res) {
+
+    const moviesKey = process.env.MOVIES_API_KEY;
+
+    let url = `https://api.themoviedb.org/3/search/multi?api_key=${moviesKey}&query=${req.params.query}`;
+    console.log(req.params.query)
+    let moviesArr = [];
+    axios
+        .get(url)
+        .then((moviesData) => {
+            console.log(moviesData.data.results)
+            moviesData.data.results.map((item) => {
+                if (item.poster_path && (+item.vote_count >= 1)) {
+                    moviesArr.push(new Movies(item))
+                }
+            });
+            res.send(moviesArr);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+}
+
+class Movies {
+    constructor(movie) {
+        (this.released_on = movie.release_date),
+            (this.title = movie.title),
+            (this.total_votes = movie.vote_count),
+            (this.image_url = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`),
+            (this.average_votes = movie.vote_average),
+            (this.overview = movie.overview),
+            (this.popularity = movie.popularity)
+    }
+}
+
 class Forecast {
     constructor(city) {
         this.date = city.datetime;
-        this.description = city.weather.description
+        this.description = `Low of ${city.low_temp} and high of ${city.max_temp}with ${city.weather.description}`;
+        this.temp = city.temp;
     }
 };
