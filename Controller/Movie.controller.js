7432558029750fddbf56b0dd741bc66e0fc331d8
renadalkhlafat <renadsalem8888@gmail.com>
2021-08-12 16:40:44 +0300

    lab10 cacheing

--- Controller/Movie.controller.js
+++ Controller/Movie.controller.js
@@ -1,26 +1,46 @@
 const axios = require('axios');
 const Movies = require('../Modale/Movies');
+const Cache = require('../Modale/Cache')
 
+let cacheObj = new Cache();
 function moviesHandler(req, res) {
 
     const moviesKey = process.env.MOVIES_API_KEY;
-
+    const key = `movies-${req.params.query}`;
     let url = `https://api.themoviedb.org/3/search/multi?api_key=${moviesKey}&query=${req.params.query}`;
     console.log(req.params.query)
     let moviesArr = [];
-    axios
-        .get(url)
-        .then((moviesData) => {
-            console.log(moviesData.data.results)
-            moviesData.data.results.map((item) => {
-                if (item.poster_path && (+item.vote_count >= 1)) {
-                    moviesArr.push(new Movies(item))
-                }
-            });
-            res.send(moviesArr);
-        })
-        .catch((error) => {
-            res.status(500).send(error);
-        });
+    if (req.params.query) {
+
+        if (cacheObj[key] && Date.now() - cacheObj[key].timeStamp < 86400000) {
+            console.log('Mfound');
+            // console.log(cacheObj[key].timeStamp);
+            // console.log(Date.now());
+            res.send(cacheObj[key].data)
+        } else {
+            console.log('M not found');
+            axios
+                .get(url)
+                .then((moviesData) => {
+                    moviesData.data.results.map((item) => {
+                        if (item.poster_path) {
+                            if (item.release_date !== 'Invalid Date') {
+                                moviesArr.push(new Movies(item))
+                            }
+                        }
+                    });
+                    cacheObj[key] = {};
+                    cacheObj[key].timeStamp = Date.now();
+                    cacheObj[key].data = moviesArr
+                    
+                    res.send(moviesArr);
+                })
+                .catch((error) => {
+                    res.status(500).send(error);
+                });
+        }
+    } else {
+        res.send("please provide the city name");
+    }
 }
 module.exports = moviesHandler;
--- Controller/Weather.controller.js
+++ Controller/Weather.controller.js
@@ -1,21 +1,42 @@
 const axios = require('axios')
-const Forecast =require('../Modale/Forecast');
+const Forecast = require('../Modale/Forecast');
+const Cache = require('../Modale/Cache')
+let cacheObj = new Cache();
 
 function handleWeather(req, res) {
+    const key = `weather-${req.params.lat}-${req.params.lon}`
     const weatherKey = process.env.WEATHER_API_KEY;
     const resUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${+req.params.lat}&lon=${+req.params.lon}&key=${weatherKey}`;
     let weatherArr = [];
-    axios
-        .get(resUrl)
-        .then((weather) => {
-            weather.data.data.map((day) => {
-                weatherArr.push(new Forecast(day));
-            });
-            res.send(weatherArr);
-        })
-        .catch((error) => {
-            res.status(500).send(error);
-        });
+    if (req.params.lat && req.params.lon) {
+
+        if (cacheObj[key] && Date.now() - cacheObj[key].timeStamp < 86400000) {
+            console.log('found');
+            // console.log(cacheObj[key].data);
+            res.json(cacheObj[key].data)
+
+        } else {
+
+            axios
+                .get(resUrl)
+                .then((weather) => {
+                    weather.data.data.map((day) => {
+                        weatherArr.push(new Forecast(day));
+                    });
+                    cacheObj[key] = {};
+                    cacheObj[key].timeStamp = Date.now();
+                    cacheObj[key].data = weatherArr
+
+                    res.send(weatherArr);
+                })
+                .catch((error) => {
+                    res.status(500).send(error);
+                });
+        }
+    } else {
+        res.send("please provide the proper lat and lon");
+    }
+
 }
 
-module.exports=handleWeather;
+module.exports = handleWeather;
